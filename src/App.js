465867eff185bfc1f498f2334759bed03faaeec0
John Christian Linaban <jcl.void@yahoo.com>
2024-08-29 19:28:49 +0800

    Responsive About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { FaReact, FaJava, FaHtml5, FaCss3Alt, FaBootstrap, FaPython, FaLinkedin, FaFacebook, FaGithub, FaArrowDown } from "react-icons/fa";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { 
+  FaReact,
+  FaJava, 
+  FaHtml5, 
+  FaCss3Alt, 
+  FaBootstrap, 
+  FaPython, 
+  FaLinkedin, 
+  FaFacebook, 
+  FaGithub
+} from "react-icons/fa";
+import { 
+  useTypewriter,
+  Cursor
+} from "react-simple-typewriter";
 import { IoLogoJavascript } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
-import { motion, useScroll } from "framer-motion";
+import { 
+  motion,
+  useScroll
+} from "framer-motion";
 
 import React from "react";
 
@@ -95,63 +111,95 @@ function About() {
   return (
     <div
       id="about-me"
-      className="flex flex-col h-full min-h-screen bg-gradient-to-br from-slate-950 to-slate-800 px-4 py-8"
+      className="flex flex-col bg-gradient-to-tr from-slate-950 to-slate-800 px-4 py-8"
     >
       {/* Section Title */}
       <section className="text-center mb-8">
-        <h1 className="text-3xl md:text-4xl text-white font-mono font-extrabold">ABOUT ME</h1>
+        <h1 className="text-3xl md:text-4xl bg-gradient-to-r from-orange-600 to-yellow-500 bg-clip-text text-transparent
+        font-mono font-extrabold">ABOUT ME</h1>
       </section>
+      <div className="flex flex-col items-center justify-center space-y-6">
 
-      {/* Technologies and Bio Section */}
-      <div className="flex flex-col md:flex-row justify-center items-start md:space-x-8 space-y-8 md:space-y-0 px-8">
-        {/* Technologies Card */}
-        <div className="flex-1 bg-slate-900 hover:bg-slate-950 hover:rounded-2xl transition-all shadow-lg rounded-md p-4 max-w-xs mx-auto md:mx-0">
-          <h2 className="text-lg md:text-xl text-white text-center font-mono font-extrabold mb-4">Technologies</h2>
-          <ul className="flex flex-wrap justify-center gap-4">
-            {[
-              <FaReact />, <FaJava />, <FaHtml5 />, <FaCss3Alt />,
-              <FaBootstrap />, <FaPython />, <IoLogoJavascript />, <RiTailwindCssFill />
-            ].map((Icon, index) => (
-              <li key={index} className="flex items-center justify-center">
-                {React.cloneElement(Icon, { size: 48, color: "white" })}
+        <div className="flex md:flex-nowrap sm:flex-wrap sm:justify-center rounded-lg shadow-xl p-2 bg-slate-950 w-[80%] md:w-[60%] sm:w-[95%] items-center">
+            <h2 className="text-xl md:text-xl bg-gradient-to-r 
+            from-purple-500 to-cyan-400 bg-clip-text text-transparent
+            font-mono font-extrabold m-6">Skill & Technologies</h2>
+            <div className="flex flex-col">
+              <ul className="flex flex-wrap items-center gap-4 m-2">
+                Programming
+                {[
+                    <FaPython />, <FaJava />
+                  ].map((Icon, index) => (
+                    <li key={index} className="flex items-center justify-center">
+                      {React.cloneElement(Icon, { size: 48, color: "white" })}
+                    </li>
+                ))}
+              </ul>
+              <ul className="flex flex-wrap items-center gap-6 m-2">
+                Web Development
+                {[
+                    <FaHtml5 />, <FaCss3Alt />, <IoLogoJavascript />,
+                    <FaReact />, <RiTailwindCssFill />, <FaBootstrap/>
+                  ].map((Icon, index) => (
+                    <li key={index} className="flex items-center justify-center">
+                      {React.cloneElement(Icon, { size: 48, color: "white" })}
+                    </li>
+                ))}
+              </ul>   
+            </div>
+          </div>
+
+        <div className="flex md:flex-nowrap sm:flex-wrap sm:justify-center rounded-lg shadow-xl p-2 bg-slate-950 w-[80%] md:w-[60%] sm:w-[95%] items-center">
+          <h2 className="text-xl md:text-xl
+          bg-gradient-to-r from-purple-500 to-cyan-400 bg-clip-text
+          text-transparent font-mono font-extrabold m-6">Experience</h2>
+          <div className="md:pl-10">
+            <ul className="flex flex-wrap items-center list-disc m-2 text-gray-400">
+              <h3 className="md:text-xl sm:text-lg bg-gradient-to-r from-orange-600 to-yellow-500 bg-clip-text text-transparent
+              font-mono font-extrabold">Commission on Audit | Information Technology Office (Intern)</h3>
+              <h4 className="md:text-lg sm:text-sm text-white font-mono font-bold">February 2024 - March 2024</h4>
+              <li>Assisted on the Development of OJT Information Management System.</li>
+              <li>
+              Upgraded the platform of sites for enhanced security. Including
+              the migration of data from old to new site, and also updating
+              the looks of the site:
+                  <ul className="list-decimal list-inside lg:indent-4 indent-0">
+                    <li>https://web.coa.gov.ph/iaro/</li>
+                    <li>https://web.coa.gov.ph/sdg/</li>
+                    <li>https://web.coa.gov.ph/imp/</li>
+                    <li>https://web.coa.gov.ph/praise/</li>
+                  </ul>
               </li>
-            ))}
-          </ul>
+            </ul>
+          </div>
         </div>
 
-        {/* Hobbies Card */}
-        <div className="flex-1 bg-slate-900 hover:bg-slate-950 hover:rounded-2xl transition-all shadow-lg rounded-md p-4 max-w-xs mx-auto md:mx-0">
-          <h2 className="text-lg md:text-xl text-center text-white font-mono font-extrabold mb-4">Hobbies</h2>
-          <ul className="text-white text-center space-y-2">
-            {["Reading", "Coding", "Gaming"].map((hobby, index) => (
-              <li key={index}>{hobby}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      {/* Experience and Certifications Section */}
-      <div className="flex flex-col md:flex-row justify-center items-start md:space-x-8 space-y-8 md:space-y-0 px-8 my-8">
-        {/* Experience Card */}
-        <div className="flex-1 bg-slate-900 hover:bg-slate-950 hover:rounded-2xl transition-all shadow-lg rounded-md p-4 max-w-xs mx-auto md:mx-0">
-          <h2 className="text-lg md:text-xl text-white text-center font-mono font-extrabold mb-4">Experience</h2>
-          {/* Add your experience content here */}
+        <div className="flex md:flex-nowrap sm:flex-wrap sm:justify-center rounded-lg shadow-xl p-2 bg-slate-950 w-[80%] md:w-[60%] sm:w-[95%] items-center">
+          <h2 className="text-xl md:text-xl bg-gradient-to-r 
+            from-purple-500 to-cyan-400 bg-clip-text text-transparent
+            font-mono font-extrabold m-6">Education</h2>
+              aoefjaopj
         </div>
 
-        {/* Certifications Card */}
-        <div className="flex-1 bg-slate-900 hover:bg-slate-950 hover:rounded-2xl transition-all shadow-lg rounded-md p-4 max-w-xs mx-auto md:mx-0">
-          <h2 className="text-lg md:text-xl text-center text-white font-mono font-extrabold mb-4">Certifications</h2>
-          {/* Add your certifications content here */}
+        <div className="flex md:flex-nowrap sm:flex-wrap sm:justify-center rounded-lg shadow-xl p-2 bg-slate-950 w-[80%] md:w-[60%] sm:w-[95%] items-center">
+          <h2 className="text-xl md:text-xl
+          bg-gradient-to-r from-purple-500 to-cyan-400 bg-clip-text text-transparent font-mono font-extrabold m-6">Certifications</h2>
+              aoefjaopj
         </div>
+
       </div>
 
       {/* Bio Section */}
       <section className="flex flex-col items-center mt-8 px-4">
         <hr className="w-2/3 h-1 my-4 bg-gray-200 border-0 rounded dark:bg-gray-700" />
-        <h2 className="text-xl md:text-2xl text-white font-mono font-extrabold mb-4">BIO</h2>
+        <h2 className="text-xl md:text-2xl bg-gradient-to-r from-orange-600 to-yellow-500 
+            bg-clip-text text-transparent
+            font-mono font-extrabold mb-4">A Short Bio...</h2>
         <div className="max-w-2xl mx-auto">
-          <p className="text-white text-base md:text-lg text-center">
-            I'm John Christian, a passionate IT student with a love for coding and technology. Currently pursuing a BSIT degree at the Polytechnic University of the Philippines.
+          <p className="text-gray-400 md:text-lg text-justify indent-4 font-mono">
+            I'm currently pursuing a Bachelor's degree in Information Technology. My journey began when I was 9 years old and first discovered programming with Visual Basic 6.0. I immediately fell in love with it! Learning how to create programs was fascinating to me, and it sparked my curiosity to explore more. Over time, I learned various programming languages and technologies. 
+
+            However, I eventually stopped actively learning programming around the age of 13, and only dabbled in it occasionally. Then, during my Work Immersion in Senior High School, my passion for programming was reignited. I resumed my studies in programming and everything related to computer science, and now I'm learning at a faster pace than ever before!
           </p>
         </div>
       </section>
@@ -161,16 +209,15 @@ function About() {
 
 function Proj() {
   return (
-    <div
-      id="proj"
-      className="bg-gradient-to-tr from-slate-950 to-slate-800 min-h-screen"
-    >
-      <div className="flex flex-col px-4 py-8">
-        <section className="text-center mb-8">
-          <h1 className="text-3xl md:text-4xl text-white font-mono font-extrabold">
+    <div id="proj" className="bg-gradient-to-br from-slate-950 to-slate-800 min-h-screen">
+      <section className="text-center mb-8 px-4 py-8">
+          <h1 className="text-3xl md:text-4xl bg-gradient-to-r from-orange-600 to-yellow-500 bg-clip-text text-transparent
+          font-mono font-extrabold">
             PROJECTS & RESEARCH
           </h1>
-        </section>
+      </section>
+      <div className="flex items-center justify-center">
+        <h2 className="text-xl text-white font-mono">(Placeholder)</h2>
       </div>
     </div>
   );
@@ -187,8 +234,8 @@ function App() {
         style={{ scaleX: scrollYProgress }}
       />
       <Hero />
-      <About />
       <Proj />
+      <About />
     </div>
   );
 }
